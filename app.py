5ffc74a929b3ddd7473506d3e7c2c4ef5499756e
Matthew Emaikwu <memaikwu@terpmai.umd.edu>
2024-03-26 22:41:57 -0400

    Within app.py, I added new routes for contact.html and favorite_course.html. Within these routes, I am able to fill out a contact page as an end user and see my results regurgitated back to me. I am also able to input course name and number into a url and hae that information gien back to me through /favorite-courses.

--- app.py
+++ app.py
@@ -1,23 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World from Matthew Emaikwu! This is my first HTML page.'
-
-@app.route('/hello')
-def hello():  # put application's code here
-    return render_template('hello.html')
-
 @app.route('/about')
-def about():  # put application's code here
+def about():
     return render_template('about.html')
 
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
 @app.route('/about-css')
-def aboutcss():  # put application's code here
+def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def course():
+    print('Course Name entered: ' + request.args.get('course_name'))
+    print('Course Number entered: ' + request.args.get('course_num'))
+
+    return render_template('favorite_course.html')
+
+@app.route('/contact' , methods=['GET' , 'POST'])
+def contact():
+    if request.method == 'POST':
+
+        first_name = request.form.get("firstnameInput1")
+        last_name = request.form.get("lastnameInput1")
+        email = request.form.get("emailInput1")
+        major = request.form.get("majorInput1")
+
+        print('Thank you for filling out the form!')
+        print('First name entered: ' + first_name)
+        print('Last name entered: ' + last_name)
+        print('Email address entered: ' + email)
+        print('University Major entered: ' + major)
+        return render_template(
+            'contact.html',
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            major=major,
+            form_submitted=True
+        )
+    else:
+        return render_template('contact.html')
+
+
 
 if __name__ == '__main__':
     app.run()
