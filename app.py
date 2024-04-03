fc4d6bb7516e6efe7c98ade8ea89e45fd1523066
Matthew Emaikwu <memaikwu@terpmai.umd.edu>
2024-04-03 18:39:26 -0400

    Copied over data from student information system database. Modified the database to include a VARCHAR(100)field to store a student's emails. Modified the student_entry and student_view files to accommodate the new email field, which ultimately makes the field a requirement when adding or editing a student in the "View All Students", "View Student", "Add New Student",  and "Update Student" pages. Initialized the tables to set up the university database.

--- app.py
+++ app.py
@@ -1,51 +1,124 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from models import db, Student, Major
+from datetime import datetime as dt
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'university.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'beyond_course_scope'
+db.init_app(app)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+# Initialize the tables
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/student/view')
+def student_view_all():
+    students = Student.query.outerjoin(Major, Student.major_id == Major.major_id) \
+        .add_entity(Major) \
+        .order_by(Student.last_name, Student.first_name) \
+        .all()
+    return render_template('student_view_all.html', students=students)
 
-@app.route('/about-css')
-def about_css():
-    return render_template('about-css.html')
 
-@app.route('/favorite-course')
-def course():
-    print('Course Name entered: ' + request.args.get('course_name'))
-    print('Course Number entered: ' + request.args.get('course_num'))
+@app.route('/student/view/<int:student_id>')
+def student_view(student_id):
+    student = Student.query.filter_by(student_id=student_id).first()
+    majors = Major.query.order_by(Major.major) \
+        .order_by(Major.major) \
+        .all()
 
-    return render_template('favorite_course.html')
+    if student:
+        return render_template('student_entry.html', student=student, majors=majors, action='read')
 
-@app.route('/contact' , methods=['GET' , 'POST'])
-def contact():
-    if request.method == 'POST':
-
-        first_name = request.form.get("firstnameInput1")
-        last_name = request.form.get("lastnameInput1")
-        email = request.form.get("emailInput1")
-        major = request.form.get("majorInput1")
-
-        print('Thank you for filling out the form!')
-        print('First name entered: ' + first_name)
-        print('Last name entered: ' + last_name)
-        print('Email address entered: ' + email)
-        print('University Major entered: ' + major)
-        return render_template(
-            'contact.html',
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            major=major,
-            form_submitted=True
-        )
     else:
-        return render_template('contact.html')
+        flash(f'Student attempting to be viewed could not be found!', 'error')
+        return redirect(url_for('student_view_all'))
 
+
+@app.route('/student/create', methods=['GET', 'POST'])
+def student_create():
+    if request.method == 'GET':
+        majors = Major.query.order_by(Major.major) \
+            .order_by(Major.major) \
+            .all()
+        return render_template('student_entry.html', majors=majors, action='create')
+    elif request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        major_id = request.form['major_id']
+
+        birth_date = request.form['birth_date']
+        is_honors = True if 'is_honors' in request.form else False
+
+        student = Student(first_name=first_name, last_name=last_name, major_id=major_id,
+                          birth_date=dt.strptime(birth_date, '%Y-%m-%d'), is_honors=is_honors)
+        db.session.add(student)
+        db.session.commit()
+        flash(f'{first_name} {last_name} was successfully added!', 'success')
+        return redirect(url_for('student_view_all'))
+
+    flash('Invalid action. Please try again.', 'error')
+    return redirect(url_for('student_view_all'))
+
+
+@app.route('/student/update/<int:student_id>', methods=['GET', 'POST'])
+def student_edit(student_id):
+    if request.method == 'GET':
+        student = Student.query.filter_by(student_id=student_id).first()
+        majors = Major.query.order_by(Major.major) \
+            .order_by(Major.major) \
+            .all()
+
+        if student:
+            return render_template('student_entry.html', student=student, majors=majors, action='update')
+
+        else:
+            flash(f'Student attempting to be edited could not be found!', 'error')
+
+    elif request.method == 'POST':
+        student = Student.query.filter_by(student_id=student_id).first()
+
+        if student:
+            student.first_name = request.form['first_name']
+            student.last_name = request.form['last_name']
+            student.major_id = request.form['major_id']
+            student.birthdate = dt.strptime(request.form['birth_date'], '%Y-%m-%d')
+            student.num_credits_completed = request.form['num_credits_completed']
+            student.gpa = request.form['gpa']
+            student.is_honors = True if 'is_honors' in request.form else False
+
+            db.session.commit()
+            flash(f'{student.first_name} {student.last_name} was successfully updated!', 'success')
+        else:
+            flash(f'Student attempting to be edited could not be found!', 'error')
+
+        return redirect(url_for('student_view_all'))
+
+    return redirect(url_for('student_view_all'))
+
+
+@app.route('/student/delete/<int:student_id>')
+def student_delete(student_id):
+    student = Student.query.filter_by(student_id=student_id).first()
+
+    if student:
+        db.session.delete(student)
+        db.session.commit()
+        flash(f'{student} was successfully deleted!', 'success')
+    else:
+        flash(f'Delete failed! Student could not be found.', 'error')
+
+    return redirect(url_for('student_view_all'))
+
+
+@app.route('/')
+def home():
+    return redirect(url_for('student_view_all'))
 
 
 if __name__ == '__main__':
